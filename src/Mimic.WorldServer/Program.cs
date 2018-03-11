8f8fc7c2f686d03f6034ee387431f6ddc1e692e7
FiniteReality <digpoealternate@gmail.com>
2018-03-11 23:45:03 +0000

    Scaffold world server project for later

---
+++ src/Mimic.WorldServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mimic.WorldServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
